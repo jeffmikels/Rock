b12ca40388577b79b2e19f07f201960b6c812593
Matt Bronkema <matt.bronkema@sparkdevnetwork.org>
2020-07-28 12:24:50 -0400

    Clean up unused packages.

---
+++ Rock.Oidc/OpenIdConnectServerDefaults.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * This code is copied from https://github.com/aspnet-contrib/AspNet.Security.OpenIdConnect.Server/blob/1.1.0/src/AspNet.Security.OpenIdConnect.Server/OpenIdConnectServerDefaults.cs
+ * to avoid having to take on all of the dependencies of AspNet.Security.OpenIdConnect.Server. We only need this one class.
+ */
+namespace AspNet.Security.OpenIdConnect.Server
+{
+    /// <summary>
+    /// Exposes the default values used by the OpenID Connect server middleware.
+    /// </summary>
+    public static class OpenIdConnectServerDefaults
+    {
+        /// <summary>
+        /// Default value for <see cref="AuthenticationOptions.AuthenticationScheme"/>.
+        /// </summary>
+        public const string AuthenticationScheme = "ASOS";
+    }
+}
